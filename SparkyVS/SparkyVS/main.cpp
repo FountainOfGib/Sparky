c8c49787db7761a150d4e1b4e27faf73e6b09774
FountainOfGib <dfo4242@gmail.com>
2022-02-24 00:21:59 +0000

    Added 2d, 3d, 4d vectors

--- SparkyVS/SparkyVS/main.cpp
+++ SparkyVS/SparkyVS/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
 #include "src/graphics/window.h"
+#include "src/maths/vec2.h"
+#include "src/maths/vec3.h"
+#include "src/maths/vec4.h"
 
 int main() 
 {
 	using namespace sparky;
 	using namespace graphics;
+	using namespace maths;
 
 	Window window("Sparky!", 800, 600);
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
@@ -14,12 +18,37 @@ int main()
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
+
+	vec2 v2a(1.0f, 2.0f);
+	vec2 v2b(3.0f, 4.0f);
+
+	vec3 v3a(1.0f, 2.0f, 3.0f);
+	vec3 v3b(3.0f, 4.0f, 5.0f);
+
+	vec4 v4a(1.0f, 2.0f, 3.0f, 4.0f);
+	vec4 v4b(3.0f, 4.0f, 5.0f, 6.0f);
+
+
 	while (!window.closed())
 	{
+		std::cout << v2a << std::endl;
+		v2a += v2b;
+		std::cout << v2a << std::endl;
+
+
+		std::cout << v3a << std::endl;
+		v3a += v3b;
+		std::cout << v3a << std::endl;
+
+
+		std::cout << v4a << std::endl;
+		v4a += v4b;
+		std::cout << v4a << std::endl;
+
 		window.clear();
-		double x, y;
-		window.getMousePosition(x, y);
-		std::cout << x << "    " << y << std::endl;
+		//double x, y;
+		//window.getMousePosition(x, y);
+		//std::cout << x << "    " << y << std::endl;
 #if 1
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
