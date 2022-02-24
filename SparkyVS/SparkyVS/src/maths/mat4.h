58bf182bc121d9a9ccb09f31bfba3e15c20be9b2
FountainOfGib <dfo4242@gmail.com>
2022-02-24 23:27:05 +0000

    Added *= operator to header file

--- SparkyVS/SparkyVS/src/maths/mat4.h
+++ SparkyVS/SparkyVS/src/maths/mat4.h
@@ -15,6 +15,7 @@ namespace sparky { namespace maths {
 
 		mat4& multiply(const mat4& other);
 		friend mat4 operator*(mat4 left, const mat4& right);
+		mat4& operator *= (const mat4& other);
 		
 		static mat4 orthographic(float left, float right, float bottom, float top, float near, float far);
 		static mat4 perspective(float fov, float aspectRatio);
